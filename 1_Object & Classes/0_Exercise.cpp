5b8ce0187d259e62a609704b96efee1cb40b31b0
Malik M Jahangir <73543873+Malik786-git@users.noreply.github.com>
2021-01-22 18:15:39 +0500

    Updated

--- 1_Object & Classes/0_Exercise.cpp
+++ 1_Object & Classes/0_Exercise.cpp
@@ -36,7 +36,7 @@
 // }
 //////////////////////////////////////////////////////////////////////////////////////////////
 
-// 2. Imagine a tollbooth at a bridge. Cars passing by the booth are expected to pay a 50 cent
+// 2. Imagine a tollbooth at a bridge. Cars passing by the booth are expected to pay a 50 cent or half $ = 0.5
 // toll. Mostly they do, but sometimes a car goes by without paying. The tollbooth keeps
 // track of the number of cars that have gone by, and of the total amount of money collected.
 // Model this tollbooth with a class called tollBooth. The two data items are a type
@@ -50,48 +50,64 @@
 // to count a paying car, and another to count a nonpaying car. Pushing the Esc key should
 // cause the program to print out the total cars and total cash and then exit.
 
-
-#include <iostream>
-using namespace std;
-#include <conio.h>
-const char ESC = 27; //escape key ASCII code
-const double TOLL = 0.5; //toll is 50 cents
-////////////////////////////////////////////////////////////////
-class tollBooth
-{
-private:
-unsigned int totalCars; //total cars passed today
-double totalCash; //total money collected today
-public: //constructor
-tollBooth() : totalCars(0), totalCash(0.0)
-{ }
-void payingCar() //a car paid
-{ totalCars++; totalCash += TOLL; }
-void nopayCar() //a car didn’t pay
-{ totalCars++; }
-void display() const //display totals
-{ cout << "\nCars=" << totalCars
-<< ", cash=" << totalCash
-<< endl; }
-};
-
-////////////////////////////////////////////////////////////////
-int main()
-{
-tollBooth booth1; //create a toll booth
-char ch;
-cout << "\nPress 0 for each non-paying car,"
-     << "\n 1 for each paying car,"
-     << "\n Esc to exit the program.\n";
-do {
-    ch = getche(); //get character
-    if( ch == '0' ) //if it’s 0, car didn’t pay
-    booth1.nopayCar();
-    if( ch == '1' ) //if it’s 1, car paid
-    booth1.payingCar();
-} while( ch != ESC ); //exit loop on Esc key
-    booth1.display(); //display totals
+// ////////////////////////////////////////////////////////////////////////////////////////////////////
+// #include <iostream>
+// using namespace std;
+// #include <conio.h>
 
 
-return 0;
-}
+// const char ESC = 27;
+
+// class tollBooth
+// {
+// private:
+//    unsigned int totalCar;
+//    double totalMoney;
+// public:
+//    tollBooth(): totalCar(0), totalMoney(0) {}
+
+//    void payingCar(){
+//        totalCar++; 
+//        totalMoney +=  0.50;
+//    }
+//    void noPayCar(){
+//        totalCar++;
+//    }
+//    void disply() const {
+//        cout<<"/nTotal Car Passes: "<<totalCar<<endl;
+//        cout<<"Total Amount of Money: "<<totalMoney<<endl;
+//    }
+// };
+
+// int main(){
+//   char ch;   
+//   tollBooth T;
+//   cout<<"Press 1 For Paying Car"<<endl
+//       <<"Press 2 For Non Pay Car"<<endl
+//       <<"Esc For Exit, Get Result "<<endl;
+
+//  do{
+//     ch = getche();
+//     if(ch=='1')
+//     {
+//         T.payingCar();
+//     }
+//     if(ch=='2')
+//     {
+//         T.noPayCar();
+//     }
+    
+//  }while(ch != ESC);
+//         T.disply();
+
+//   return 0;
+// }
+
+
+
+
+
+
+
+
+
